10f64cd152cbcae2973e49d1bf1bd7fd72102e3c
julymoe <athelay.lwin@gmail.com>
2022-06-05 20:55:09 +0630

    font

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import Contact from "../components/Contact";
 import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import SkillTool from "../components/SkillTool";
-import Work from "../components/Work";
-import Experience from "../components/Experience";
 import styles from "../styles/Home.module.css";
 
 
@@ -16,8 +10,7 @@ export default function Home() {
         <title>My Portfolio</title>
         <meta name="description" content="Portfolio" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.ico" />
-        
+        <link rel="icon" href="/favicon.ico" />        
       </Head>
       <main>
         <Hero />
