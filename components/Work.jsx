d2286e3f0e619cba58b1148e9c76c5dc8fad5f8a
julymoe <athelay.lwin@gmail.com>
2022-06-07 09:28:49 +0630

    404

--- components/Work.jsx
+++ components/Work.jsx
@@ -134,9 +134,6 @@ const Work = () => {
                   #Framer Motion
                 </span>
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #Emailjs
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                   #Netlify
                 </span>
               </div>
